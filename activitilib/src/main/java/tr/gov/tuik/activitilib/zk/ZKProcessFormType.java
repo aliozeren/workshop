4f746b685f54a1bd7c05af39cd7ab8d79fd718dd
baranchn <baranchn@gmail.com>
2016-11-30 11:16:11 +0200

    ZK Process Form type added

---
+++ activitilib/src/main/java/tr/gov/tuik/activitilib/zk/ZKProcessFormType.java
@@ -0,0 +1,22 @@
+package tr.gov.tuik.activitilib.zk;
+
+import org.activiti.engine.form.FormProperty;
+import org.zkoss.zul.Textbox;
+
+import tr.gov.tuik.activitilib.types.AbstractProcessFormType;
+
+public class ZKProcessFormType extends AbstractProcessFormType  
+{
+
+	private static final long serialVersionUID = 3868249214623992954L;
+
+
+	public DynamicModel renderInput(FormProperty property) 
+	{
+		Textbox component = new Textbox();
+		component= (Textbox) ZKInputUtils.getInstance().createHtmlBasedComponent(component, this);
+		
+		return  ZKInputUtils.getInstance().getDynamicModel(this, component);
+	}
+	
+}
