cfc509e829e8026674b1073ec918a8d3affcd662
ali.ozeren <ali.ozeren@agem.com.tr>
2016-12-01 14:29:04 +0300

    process form type is updated

--- activitilib/src/main/java/tr/gov/tuik/activitilib/zk/ZKProcessFormType.java
+++ activitilib/src/main/java/tr/gov/tuik/activitilib/zk/ZKProcessFormType.java
@@ -11,10 +11,14 @@ public class ZKProcessFormType extends AbstractProcessFormType
 	private static final long serialVersionUID = 3868249214623992954L;
 
 
-	public DynamicModel renderInput(FormProperty property) 
+	public DynamicModel renderInput(FormProperty property, String value) 
 	{
 		Textbox component = new Textbox();
 		component= (Textbox) ZKInputUtils.getInstance().createHtmlBasedComponent(component, this);
+		
+		if (value != null) {
+			component.setValue(value);
+		}
 		
 		return  ZKInputUtils.getInstance().getDynamicModel(this, component);
 	}
